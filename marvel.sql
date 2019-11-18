d8834bf65101e884d148c6d71df9bc5554dbfb0f
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-18 18:43:40 +0000

    Removes SELECT * FROM movies at end of file

--- marvel.sql
+++ marvel.sql
@@ -77,4 +77,3 @@ INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 20
 -- The cinema would like to make the Iron Man movies a triple billing. Find out the show time of “Iron Man 2” and set the show time of “Iron Man 3” to start two hours later.
 SELECT show_time FROM movies WHERE title = 'Iron Man 2';
 UPDATE movies SET show_time = '20:45' WHERE title = 'Iron Man 3';
-SELECT * FROM movies;
