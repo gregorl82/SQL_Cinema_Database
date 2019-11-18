64610a9eaf6ee00b4cc5ebfacdc0bfaaab7b7029
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-18 18:19:14 +0000

    Updates 'Crusty the Clown' to 'Krusty the Clown' in the people table

--- marvel.sql
+++ marvel.sql
@@ -52,8 +52,13 @@ INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:0
 
 -- Return ALL the data in the ‘movies’ table.
 SELECT * FROM movies;
+
 -- Return ONLY the name column from the ‘people’ table
+SELECT name FROM people;
+
 -- Oops! Someone spelled Krusty The Clown’s name wrong! Change it to reflect the proper spelling (Crusty should be Krusty).
+UPDATE people SET name = 'Krusty the Clown' WHERE name = 'Crusty the Clown';
+
 -- Return ONLY Homer Simpson’s name from the ‘people’ table.
 -- The cinema is showing ‘Batman Begins’, but Batman is DC, not Marvel! Delete the entry from the ‘movies’ table.
 -- We forgot one of the main characters! Add Bart Simpson to the ‘people’ table
